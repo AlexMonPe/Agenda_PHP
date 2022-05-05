eb300e118555c43f76924afb18d0a6c167db8b87
Alejandro <Alex_bcn10@hotmail.es>
2022-05-05 21:15:14 +0200

    feat: created post contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,28 +10,53 @@ class ContactController extends Controller
 {
     public function getAllContacts()
     {
-        $contacts = DB::table('contacts')->where('id_user',"=", 1)->get()->toArray();
+        // $contacts = DB::table('contacts')
+        // ->select('name', 'surname as apellido')
+        // ->where('id_user',"=", 1)
+        // ->get()
+        // ->toArray();
 
-        return $contacts;
+        $contacts = Contact::where('id_user', 1)->get()->toArray();
+
+        return response()->json(["success" => "response success"],200);
     }
     
     public function getContactById($id)
     {
-        $contact = DB::table('contacts')->where('id_user',"=", 1)->where('id', "=", $id)->get()->toArray();
-        $contact = DB::table('contacts')->where('id_user', "=", 1)->find($id);
+        // $contact = DB::table('contacts')->where('id_user',"=", 1)->where('id', "=", $id)->get()->toArray();
+        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
 
-        return 'GET CONTACT BY ID ->'.$id;
+        if(empty($contact)) {
+            return response()->json([
+                "error" => "Contact not found"
+            ]
+            , 404);
+        }
+
+        return response()->json($contact,200);
     }
 
     public function createContact(Request $request)
     {
-        dump($request->input('name'));
-        return 'CREATE CONTACT';
+        $newContact = new Contact();
+
+        $newContact->name = $request->name;
+        $newContact->surname = $request->surname;
+        $newContact->email = $request->email;
+        $newContact->phone_number = $request->phone_number;
+        $newContact->id_user = $request->id_user;
+
+
+        $newContact->save();
+
+        return response()->json(["data" => $newContact, "success" => "Contact created"],200);
     }
 
     public function updateContact(Request $request, $id)
     {
-        dump($request->input('name'));
+        
+
+        
         return 'update CONTACT by id: '.$id;
     }
 
