1c2b8064994b6fbf69c44ce348c6addf0943cded
Alejandro <Alex_bcn10@hotmail.es>
2022-05-06 19:07:45 +0200

    feat: added try catch in all controllers of contactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,50 +6,71 @@ use App\Models\Contact;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
     public function getAllContacts()
     {
-        // $contacts = DB::table('contacts')
-        // ->select('name', 'surname as apellido')
-        // ->where('id_user',"=", 1)
-        // ->get()
-        // ->toArray();
-        $userId = auth()->user()->id;
+        try {
+            Log::info('init get all contacts');
+            // $contacts = DB::table('contacts')
+            // ->select('name', 'surname as apellido')
+            // ->where('id_user',"=", 1)
+            // ->get()
+            // ->toArray();
+            $userId = auth()->user()->id;
 
-        // $contacts = Contact::where('id_user', 1)->get()->toArray();
+            // $contacts = Contact::where('id_user', 1)->get()->toArray();
 
-        //trae los contactos del usuario $userId
-        //$user = User::find($userId)->contacts;
+            //trae los contactos del usuario $userId
+            //$user = User::find($userId)->contacts;
 
-        //trae el usuario del contacto 7
-        $contacts = Contact::find(7)->user;
+            //trae el usuario del contacto 7
+            $contacts = Contact::find(7)->user;
 
-        return response()->json(["data" => $contacts, "success" => "response success"], 200);
+            return response()->json(["data" => $contacts, "success" => "response success"], 200);
+        } catch (\Throwable $th) {
+            Log::error('Error en get all contacts ' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error al obtener todos los contactos'
+            ], 500);
+        }
     }
 
     public function getContactById($id)
     {
-        // $contact = DB::table('contacts')->where('id_user',"=", 1)->where('id', "=", $id)->get()->toArray();
-        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+        try {
+            Log::info('init get contacts by id');
+            // $contact = DB::table('contacts')->where('id_user',"=", 1)->where('id', "=", $id)->get()->toArray();
+            $contact = Contact::where('id', $id)->where('id_user', 1)->first();
 
-        if (empty($contact)) {
-            return response()->json(
-                [
-                    "error" => "Contact not found"
-                ],
-                404
-            );
+            if (empty($contact)) {
+                return response()->json(
+                    [
+                        "error" => "Contact not found"
+                    ],
+                    404
+                );
+            }
+
+            return response()->json($contact, 200);
+        } catch (\Throwable $th) {
+            Log::error('Error en get contact by id ' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error al obtener el contacto por id'
+            ], 500);
         }
-
-        return response()->json($contact, 200);
     }
 
     public function createContact(Request $request)
     {
         try {
+            Log::info('init create contact');
+
             $newContact = new Contact();
 
             $validator = Validator::make($request->all(), [
@@ -75,53 +96,73 @@ class ContactController extends Controller
 
             return response()->json(["data" => $newContact, "success" => "Contact created"], 200);
         } catch (\Throwable $th) {
-            return 'Error';
+            Log::error('Error en create contact' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error al crear un nuevo contacto'
+            ], 500);
         }
     }
 
     public function updateContact(Request $request, $id)
     {
-        $contact = Contact::where('id', $id)->where('id_user', 1)->firstOrFail();
+        try {
+            $contact = Contact::where('id', $id)->where('id_user', 1)->firstOrFail();
 
-        if (empty($contact)) {
-            return response()->json(
-                [
-                    "error" => "Contact not found"
-                ],
-                404
-            );
-        }
+            if (empty($contact)) {
+                return response()->json(
+                    [
+                        "error" => "Contact not found"
+                    ],
+                    404
+                );
+            }
 
-        if (isset($request->name)) {
-            $contact->name = $request->name;
+            if (isset($request->name)) {
+                $contact->name = $request->name;
+            }
+            if (isset($request->surname)) {
+                $contact->surname = $request->surname;
+            }
+            if (isset($request->email)) {
+                $contact->email = $request->email;
+            }
+            if (isset($request->phone_number)) {
+                $contact->phone_number = $request->phone_number;
+            }
+            return response()->json(["data" => $contact, "success" => "Contact updated"], 200);
+        } catch (\Throwable $th) {
+            Log::error('Error en update contact by id ' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error al actualizar el contacto por id'
+            ], 500);
         }
-        if (isset($request->surname)) {
-            $contact->surname = $request->surname;
-        }
-        if (isset($request->email)) {
-            $contact->email = $request->email;
-        }
-        if (isset($request->phone_number)) {
-            $contact->phone_number = $request->phone_number;
-        }
-        return response()->json(["data" => $contact, "success" => "Contact updated"], 200);
     }
 
     public function deleteContact($id)
     {
-        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+        try {
+            $contact = Contact::where('id', $id)->where('id_user', 1)->first();
 
-        if (empty($contact)) {
-            return response()->json(
-                [
-                    "error" => "Contact not found"
-                ],
-                404
-            );
+            if (empty($contact)) {
+                return response()->json(
+                    [
+                        "error" => "Contact not found"
+                    ],
+                    404
+                );
+            }
+
+            $contact->delete();
+
+            return response()->json(["data" => $contact, "success" => "Contact deleted"], 200);;
+        } catch (\Throwable $th) {
+            Log::error('Error in delete contact by id ' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error al borrar el contacto por id'
+            ], 500);
         }
-
-        $contact->delete();
-
-        return response()->json(["data" => $contact, "success" => "Contact deleted"], 200);;
     }
 }
