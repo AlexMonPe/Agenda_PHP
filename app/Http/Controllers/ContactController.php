5369f0a8619a269c1bb9779245cfb97570c5e539
Alejandro <Alex_bcn10@hotmail.es>
2022-05-06 18:33:35 +0200

    feat: validator in contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
@@ -15,53 +17,111 @@ class ContactController extends Controller
         // ->where('id_user',"=", 1)
         // ->get()
         // ->toArray();
+        $userId = auth()->user()->id;
 
-        $contacts = Contact::where('id_user', 1)->get()->toArray();
+        // $contacts = Contact::where('id_user', 1)->get()->toArray();
 
-        return response()->json(["success" => "response success"],200);
+        //trae los contactos del usuario $userId
+        //$user = User::find($userId)->contacts;
+
+        //trae el usuario del contacto 7
+        $contacts = Contact::find(7)->user;
+
+        return response()->json(["data" => $contacts, "success" => "response success"], 200);
     }
-    
+
     public function getContactById($id)
     {
         // $contact = DB::table('contacts')->where('id_user',"=", 1)->where('id', "=", $id)->get()->toArray();
         $contact = Contact::where('id', $id)->where('id_user', 1)->first();
 
-        if(empty($contact)) {
-            return response()->json([
-                "error" => "Contact not found"
-            ]
-            , 404);
+        if (empty($contact)) {
+            return response()->json(
+                [
+                    "error" => "Contact not found"
+                ],
+                404
+            );
         }
 
-        return response()->json($contact,200);
+        return response()->json($contact, 200);
     }
 
     public function createContact(Request $request)
     {
-        $newContact = new Contact();
+        try {
+            $newContact = new Contact();
 
-        $newContact->name = $request->name;
-        $newContact->surname = $request->surname;
-        $newContact->email = $request->email;
-        $newContact->phone_number = $request->phone_number;
-        $newContact->id_user = $request->id_user;
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'surname' => 'required|string',
+                'email' => 'required|email',
+                'phone_number' => 'required|string',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
 
 
-        $newContact->save();
+            $newContact->name = $request->name;
+            $newContact->surname = $request->surname;
+            $newContact->email = $request->email;
+            $newContact->phone_number = $request->phone_number;
+            $newContact->id_user = $request->id_user;
 
-        return response()->json(["data" => $newContact, "success" => "Contact created"],200);
+
+            $newContact->save();
+
+            return response()->json(["data" => $newContact, "success" => "Contact created"], 200);
+        } catch (\Throwable $th) {
+            return 'Error';
+        }
     }
 
     public function updateContact(Request $request, $id)
     {
-        
+        $contact = Contact::where('id', $id)->where('id_user', 1)->firstOrFail();
 
-        
-        return 'update CONTACT by id: '.$id;
+        if (empty($contact)) {
+            return response()->json(
+                [
+                    "error" => "Contact not found"
+                ],
+                404
+            );
+        }
+
+        if (isset($request->name)) {
+            $contact->name = $request->name;
+        }
+        if (isset($request->surname)) {
+            $contact->surname = $request->surname;
+        }
+        if (isset($request->email)) {
+            $contact->email = $request->email;
+        }
+        if (isset($request->phone_number)) {
+            $contact->phone_number = $request->phone_number;
+        }
+        return response()->json(["data" => $contact, "success" => "Contact updated"], 200);
     }
 
     public function deleteContact($id)
     {
-        return 'DELETE CONTACT by id: '.$id;
-    }    
+        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+
+        if (empty($contact)) {
+            return response()->json(
+                [
+                    "error" => "Contact not found"
+                ],
+                404
+            );
+        }
+
+        $contact->delete();
+
+        return response()->json(["data" => $contact, "success" => "Contact deleted"], 200);;
+    }
 }
