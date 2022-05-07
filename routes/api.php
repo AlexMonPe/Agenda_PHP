a90f645da34fd46389cbf0400536745e387afabe
Alejandro <Alex_bcn10@hotmail.es>
2022-05-07 12:10:31 +0200

    feat: routes auth in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -19,6 +20,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//auth
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+//groups necesita un token para acceder a esos endpoints
+Route::group([
+'middleware' => 'jwt.auth'
+], function () {
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/profile', [AuthController::class, 'profile']);
+});
+
+
 
 Route::get('/contacts', [ContactController::class, 'getAllContacts']);
 
